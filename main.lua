28d63647d83bde73b017d45cf405818fc3ad6928
lucasm08 <gompou16@univmetiers.ci>
2018-02-08 06:20:59 +0000

    implement the start state of the game

--- main.lua
+++ main.lua
@@ -21,7 +21,11 @@ PADDLE_SPEED = 200
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest') 
 
-	love.window .setTitle('Pong')
+	love.window.setTitle('Pong')
+
+	smallFont = love.graphics.newFont('font.ttf', 8)
+    largeFont = love.graphics.newFont('font.ttf', 16)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -31,7 +35,14 @@ function love.load()
 
 	player1 = Paddle(10, 30, 5, 20)
 	player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
-	ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)	
+	ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+	player1Score = 0
+
+	player2Score = 0
+
+
+	gameState = "start"
 
 end	
 
@@ -78,9 +89,29 @@ function love.draw()
 
 	love.graphics.clear(40, 45, 52, 255)
 
+	displayScore()
+
+	
+	if gameState == "start" then
+		love.graphics.setFont(smallFont)
+		love.graphics.printf('Welcome to Pong!', 0, 5, VIRTUAL_WIDTH, 'center')
+		love.graphics.printf('Press Enter to begin', 0, 15, VIRTUAL_WIDTH, 'center')
+
+	end
+
+
+
 	player1:render()
 	player2:render()
 	ball:render()
 
 	push:apply('end')
+end
+
+function displayScore()
+
+	love.graphics.setFont(scoreFont)
+	love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+	love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 32, VIRTUAL_HEIGHT / 3)
+	-- body
 end
