3c2b628e6f74852629b91bef93e7d9f311f1bbf5
lucasm08 <gompou16@univmetiers.ci>
2018-02-09 23:37:29 +0000

    fix a mistake in the spelling of "height"

--- Paddle.lua
+++ Paddle.lua
@@ -1,11 +1,11 @@
 Paddle = Class{}
   
 
-function Paddle:init(x, y, width, heigth)
+function Paddle:init(x, y, width, height)
 	self.x = x
 	self.y = y
 	self.width = width
-	self.heigth = heigth
+	self.height= height
 	self.dy = 0
 	
 end
@@ -15,11 +15,11 @@ function Paddle:update(dt)
 	if self.dy < 0 then
 		self.y = math.max(0, self.y + self.dy * dt)
 	else
-		self.y = math.min(VIRTUAL_HEIGHT - self.heigth, self.y + self.dy * dt)
+		self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
 
 	end
 end
 
 function Paddle:render()
-	love.graphics.rectangle('fill', self.x, self.y, self.width, self.heigth)
+	love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
